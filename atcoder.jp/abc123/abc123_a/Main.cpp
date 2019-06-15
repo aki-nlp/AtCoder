bd2a04083f9316eacd216cf62f47b6f98fd98411
baum <akiuo.ou@gmail.com>
2019-06-16 01:42:35 +0900

    [AC] abc123 abc123_a

---
+++ atcoder.jp/abc123/abc123_a/Main.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a, b, c, d, e, k;
+  bool flag=true;
+  cin >> a >> b >> c >> d >> e >> k;
+  
+  if(e-a>k) cout << ":(" << endl;
+  else cout << "Yay!" << endl;
+}
