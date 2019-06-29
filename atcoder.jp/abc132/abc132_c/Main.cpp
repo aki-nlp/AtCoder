1e0e9ccade0c83eec03be40e0e13d5fc3af1d98a
baum <akiuo.ou@gmail.com>
2019-06-29 22:00:35 +0900

    [AC] abc132 abc132_c

---
+++ atcoder.jp/abc132/abc132_c/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n,r=0; 
+  cin >> n;
+  vector<int> a(n);
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }  
+  sort(a.begin(),a.end());
+  
+  cout<<a[n/2]-a[n/2-1]<<endl;
+}
