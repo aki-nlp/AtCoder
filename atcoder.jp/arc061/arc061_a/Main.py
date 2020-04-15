6f91fec810d6ad70c9c67230e8376ae0a7212769
baum <akiuo.ou@gmail.com>
2020-04-16 03:56:03 +0900

    [AC] arc061 arc061_a

---
+++ atcoder.jp/arc061/arc061_a/Main.py
@@ -0,0 +1,13 @@
+s=input()
+n=len(s)
+ans=0
+
+for bit in range(1<<(n-1)):
+    f=s[0]
+    for i in range(n-1):
+        if bit&(1<<i):
+            f+='+'
+        f+=s[i+1]
+    ans+=sum (map(int, f.split('+')))
+    
+print(ans)
