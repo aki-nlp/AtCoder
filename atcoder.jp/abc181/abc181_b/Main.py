74bfa4c0fcb61d5c39b0f2f18c54f2acfa2bbb52
baum <akiuo.ou@gmail.com>
2020-11-01 21:05:42 +0900

    [AC] abc181 abc181_b

---
+++ atcoder.jp/abc181/abc181_b/Main.py
@@ -0,0 +1,14 @@
+def main():
+    n = int(input())
+    ans = 0
+    
+    for _ in range(n):
+        a, b = map(int, input().split())
+        a -= 1
+        ans += (b*(b+1)//2) -(a*(a+1)//2)
+        
+    print(ans)
+    
+        
+if __name__ == '__main__':
+    main()
